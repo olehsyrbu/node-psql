d2fda64b7c0a37ffcc76cd497d259ddc3c2fa732
OlehSirbu <oleh@bookinglayer.com>
2024-05-16 17:44:48 +0200

    chore: Update database configuration and controller code

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "123",
-  DB: "testdb",
+  USER: "oleh",
+  PASSWORD: "1234",
+  DB: "postgres",
   dialect: "postgres",
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000
-  }
+    idle: 10000,
+  },
 };
